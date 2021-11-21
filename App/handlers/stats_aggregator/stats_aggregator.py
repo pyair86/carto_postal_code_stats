149c163c9bc74cb947c5ae532ae01ce1b39c75ae
ya pr <1562330+pyair86@users.noreply.github.com>
2021-11-21 04:17:32 +0200

    add extra endpoints for population statistics aggregated by column from the stats table

--- App/handlers/stats_aggregator/stats_aggregator.py
+++ App/handlers/stats_aggregator/stats_aggregator.py
@@ -1,4 +1,9 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
 class StatsPolygonAggregator:
+
     """
     Responsible for aggregating the statistical and integration with the
     geojson.
@@ -6,13 +11,21 @@ class StatsPolygonAggregator:
     Inner loop in aggregate_data should not be that bad
     because usually there aren't so many columns in a table.
     """
+
     def __init__(
-        self, cursor, geo_json_query, agg_postal_code_data_query, postal_code=None
-    ):
+        self,
+        cursor,
+        geo_json_query,
+        agg_postal_code_data_query,
+        postal_code=None,
+        aggregator_column='p_age',
+        ):
+
         self.cursor = cursor
         self.postal_code = postal_code
         self.geo_json_query = geo_json_query
         self.agg_postal_code_data_query = agg_postal_code_data_query
+        self.aggregator_column = aggregator_column
 
     @staticmethod
     def aggregate_data(data_rows, column_names):
@@ -22,7 +35,7 @@ class StatsPolygonAggregator:
 
             statistics = {}
 
-            for index, column_name in enumerate(column_names):
+            for (index, column_name) in enumerate(column_names):
                 statistics[column_name] = data_row[index]
 
             aggregated_result.append(statistics)
@@ -30,38 +43,76 @@ class StatsPolygonAggregator:
         return aggregated_result
 
     def get_geojson_geom_coordinates(self):
-        self.cursor.execute(self.geo_json_query, (self.postal_code,))
+        self.cursor.execute(self.geo_json_query, (self.postal_code, ))
         query_result = self.cursor.fetchone()
         if query_result:
             geojson = query_result[0]
-
             return geojson
+        return False
 
     def get_postal_code_stats(self):
-        self.cursor.execute(self.agg_postal_code_data_query, (self.postal_code,))
+        self.cursor.execute(self.agg_postal_code_data_query,
+                            (self.postal_code, ))
         postal_code_stats = self.cursor.fetchall()
         return postal_code_stats
 
     def get_geojson_aggregated_stats(self):
-
+        collected_stats_json_agg_column = None
         geojson = self.get_geojson_geom_coordinates()
-
         postal_code_stats = self.get_postal_code_stats()
         if not postal_code_stats:
             return False
-        db_postal_code_column_names = [desc[0] for desc in self.cursor.description]
+        db_postal_code_column_names = [desc[0] for desc in
+                self.cursor.description]
 
-        collected_stats_json = self.aggregate_data(
-            postal_code_stats, db_postal_code_column_names
-        )
+        collected_stats_json = self.aggregate_data(postal_code_stats,
+                db_postal_code_column_names)
 
-        geo_json_properties = geojson["features"][0]["properties"]
+        if self.aggregator_column:
+            collected_stats_json_agg_column = \
+                self.aggregate_stats_per_column(collected_stats_json)
 
-        geo_json_properties["population_stats"] = collected_stats_json
+        geo_json_properties = geojson['features'][0]['properties']
+        if not self.aggregator_column:
+            geo_json_properties['population_stats'] = \
+                collected_stats_json
+        else:
+            geo_json_properties['population_stats'] = \
+                collected_stats_json_agg_column
 
         # avoid duplications of coordinates in json - not necessary in properties
-        del geo_json_properties["the_geom"]
+
+        del geo_json_properties['the_geom']
+
         # id should not be so relevant for frontend
-        del geo_json_properties["id"]
+
+        del geo_json_properties['id']
 
         return geojson
+
+    def set_column_aggregator(self, population_stats):
+        result = []
+
+        for stat in population_stats:
+            new_agg_column = stat[self.aggregator_column]
+            del stat[self.aggregator_column]
+            result.append({new_agg_column: stat})
+
+        return result
+
+    def aggregate_stats_per_column(self, population_stats):
+        aggregated_stats = {}
+        stats_with_column_aggregator = \
+            self.set_column_aggregator(population_stats)
+        for (index, stat) in enumerate(stats_with_column_aggregator):
+            for (agg_column_value, ungrouped_stats) in stat.items():
+                if index < len(stats_with_column_aggregator) - 1:
+                    if agg_column_value \
+                        in stats_with_column_aggregator[index + 1]:
+                        aggregated_stats[agg_column_value] = \
+                            [ungrouped_stats,
+                             stats_with_column_aggregator[index
+                             + 1][agg_column_value]]
+
+        result = {self.aggregator_column: aggregated_stats}
+        return result
--- App/main.py
+++ App/main.py
@@ -1,13 +1,8 @@
-from flask import (
-    Flask,
-    render_template,
-    request,
-    redirect,
-    flash,
-    url_for,
-    session,
-    jsonify,
-)
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from flask import Flask, render_template, request, redirect, flash, \
+    url_for, session, jsonify
+
 from flask_caching import Cache
 from functools import wraps
 from App.forms.register_form import RegistrationForm
@@ -22,26 +17,28 @@ from App.database_py_files.db_connection_manager import DBConnectionManager
 
 # todo add readme and tests
 
-conf_json_reader = ConfJsonReader("/myApp/App/config_files/flask_config.json")
-flask_config = conf_json_reader.get_config("config")
+conf_json_reader = \
+    ConfJsonReader('/myApp/App/config_files/flask_config.json')
+flask_config = conf_json_reader.get_config('config')
 app = Flask(__name__)
 app.config.from_mapping(flask_config)
 cache = Cache(app)
-conf_json_reader = ConfJsonReader("/myApp/App/config_files/db_credentials_config.json")
+conf_json_reader = \
+    ConfJsonReader('/myApp/App/config_files/db_credentials_config.json')
 db_connector = DBConnector(conf_json_reader, DBConnectionManager)
 
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template("404.html"), 404
+    return render_template('404.html'), 404
 
 
-@app.route("/")
+@app.route('/')
 def index():
-    return render_template("main.html")
+    return render_template('main.html')
 
 
-@app.route("/register", methods=["GET", "POST"])
+@app.route('/register', methods=['GET', 'POST'])
 def register():
 
     form = RegistrationForm(request.form)
@@ -49,21 +46,22 @@ def register():
 
     try:
         db_connection = db_connector.connect()
-        if request.method == "POST" and form.validate():
-            RegisterHandler(form, db_connection.get_cursor()).register_user()
-            flash("Thanks for registering!", "success")
-            return redirect(url_for("index"))
-        return render_template("register.html", form=form)
-
+        if request.method == 'POST' and form.validate():
+            RegisterHandler(form,
+                            db_connection.get_cursor()).register_user()
+            flash('Thanks for registering!', 'success')
+            return redirect(url_for('index'))
+        return render_template('register.html', form=form)
     except:
-        flash("ERROR!".format(form.email.data), "danger")
-        return render_template("register.html", form=form)
 
+        flash('ERROR!'.format(form.email.data), 'danger')
+        return render_template('register.html', form=form)
     finally:
+
         db_connection.close_connection()
 
 
-@app.route("/login", methods=["GET", "POST"])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
 
     form = LoginForm(request.form)
@@ -72,51 +70,51 @@ def login():
     try:
         db_connection = db_connector.connect()
 
-        if request.method == "POST" and form.validate():
+        if request.method == 'POST' and form.validate():
 
             login_handler = LoginHandler(db_connection.get_cursor())
             logged_in = login_handler.login()
             if logged_in:
-                flash("You are now logged in", "success")
-                return redirect(url_for("index"))
-
+                flash('You are now logged in', 'success')
+                return redirect(url_for('index'))
             else:
-                flash("Wrong email/password", "danger")
-                return render_template("login.html"), 401
 
-        return render_template("login.html")
+                flash('Wrong email/password', 'danger')
+                return render_template('login.html'), 401
 
+        return render_template('login.html')
     except:
-        flash("ERROR!".format(form.email.data), "danger")
-        return render_template("login.html", form=form), 401
 
+        flash('ERROR!'.format(form.email.data), 'danger')
+        return render_template('login.html', form=form), 401
     finally:
+
         db_connection.close_connection()
 
 
 def is_logged_in(f):
+
     @wraps(f)
     def wrap(*args, **kwargs):
-        if "logged_in" in session:
+        if 'logged_in' in session:
             return f(*args, **kwargs)
         else:
-            flash("Unauthorized, Please login", "danger")
-            return redirect(url_for("login"))
+            flash('Unauthorized, Please login', 'danger')
+            return redirect(url_for('login'))
 
     return wrap
 
 
-@app.route("/logout/")
+@app.route('/logout/')
 @is_logged_in
 def logout():
     cache.clear()
     session.clear()
-    flash("You are now logged out", "success")
-    return redirect(url_for("index"))
+    flash('You are now logged out', 'success')
+    return redirect(url_for('index'))
 
 
-
-@app.route("/postal_codes/<string:postal_code>")
+@app.route('/postal_codes/<string:postal_code>')
 @cache.cached()
 @is_logged_in
 def get_postal_code_stats(postal_code):
@@ -126,28 +124,27 @@ def get_postal_code_stats(postal_code):
     try:
         db_connection = db_connector.connect()
         sql_file_reader = SqlFileRuntimeReader()
-        (
-            get_geojson_single_polygon_query,
-            get_aggregated_turnover_single_polygon_query,
-        ) = sql_file_reader.read_select_single_geom_queries()
 
-        stats_polygon_aggregator = StatsPolygonAggregator(
-            db_connection.get_cursor(),
-            get_geojson_single_polygon_query,
-            get_aggregated_turnover_single_polygon_query,
-            postal_code,
-        )
-        geojson = stats_polygon_aggregator.get_geojson_aggregated_stats()
+        (get_geojson_single_polygon_query,
+         get_aggregated_turnover_single_polygon_query) = \
+            sql_file_reader.read_select_single_geom_queries()
+
+        stats_polygon_aggregator = \
+            StatsPolygonAggregator(db_connection.get_cursor(),
+                                   get_geojson_single_polygon_query,
+                                   get_aggregated_turnover_single_polygon_query,
+                                   postal_code)
+        geojson = \
+            stats_polygon_aggregator.get_geojson_aggregated_stats()
         if geojson:
             return jsonify(geojson)
-        return render_template("404.html"), 404
-
+        return render_template('404.html'), 404
     finally:
+
         db_connection.close_connection()
 
 
-
-@app.route("/postal_codes/")
+@app.route('/postal_codes/')
 @cache.cached()
 @is_logged_in
 def collect_features():
@@ -157,23 +154,84 @@ def collect_features():
         db_connection = db_connector.connect()
 
         sql_file_reader = SqlFileRuntimeReader()
-        (
-            get_geojson_collected_polygon_query,
-            get_aggregated_turnover_all_polygons_query,
-        ) = sql_file_reader.read_select_collected_geom_queries()
 
-        stats_polygon_aggregator = StatsPolygonAggregator(
-            db_connection.get_cursor(),
-            get_geojson_collected_polygon_query,
-            get_aggregated_turnover_all_polygons_query,
-        )
-        geojson = stats_polygon_aggregator.get_geojson_aggregated_stats()
+        (get_geojson_collected_polygon_query,
+         get_aggregated_turnover_all_polygons_query) = \
+            sql_file_reader.read_select_collected_geom_queries()
+
+        stats_polygon_aggregator = \
+            StatsPolygonAggregator(db_connection.get_cursor(),
+                                   get_geojson_collected_polygon_query,
+                                   get_aggregated_turnover_all_polygons_query)
+        geojson = \
+            stats_polygon_aggregator.get_geojson_aggregated_stats()
         if geojson:
             return jsonify(geojson)
-        return render_template("404.html"), 404
-
+        return render_template('404.html'), 404
     finally:
+
         db_connection.close_connection()
 
-if __name__ == "__main__":
+
+@app.route('/postal_codes_agg/<string:postal_code>')
+@cache.cached()
+@is_logged_in
+def get_postal_code_stats_agg(postal_code):
+
+    db_connection = None
+
+    try:
+        db_connection = db_connector.connect()
+        sql_file_reader = SqlFileRuntimeReader()
+
+        (get_geojson_single_polygon_query,
+         get_aggregated_turnover_single_polygon_query) = \
+            sql_file_reader.read_select_single_geom_queries()
+
+        stats_polygon_aggregator = \
+            StatsPolygonAggregator(db_connection.get_cursor(),
+                                   get_geojson_single_polygon_query,
+                                   get_aggregated_turnover_single_polygon_query,
+                                   postal_code, 'p_age')
+        geojson = \
+            stats_polygon_aggregator.get_geojson_aggregated_stats()
+        if geojson:
+            return jsonify(geojson)
+        return render_template('404.html'), 404
+    finally:
+
+        db_connection.close_connection()
+
+
+@app.route('/postal_codes_agg/')
+@cache.cached()
+@is_logged_in
+def collect_features_agg():
+    db_connection = None
+
+    try:
+        db_connection = db_connector.connect()
+
+        sql_file_reader = SqlFileRuntimeReader()
+
+        (get_geojson_collected_polygon_query,
+         get_aggregated_turnover_all_polygons_query) = \
+            sql_file_reader.read_select_collected_geom_queries()
+
+        stats_polygon_aggregator = \
+            StatsPolygonAggregator(db_connection.get_cursor(),
+                                   get_geojson_collected_polygon_query,
+                                   get_aggregated_turnover_all_polygons_query,
+                                   aggregator_column='p_age')
+        geojson = \
+            stats_polygon_aggregator.get_geojson_aggregated_stats()
+        if geojson:
+            return jsonify(geojson)
+        return render_template('404.html'), 404
+    finally:
+
+        db_connection.close_connection()
+
+
+if __name__ == '__main__':
     app.run()
